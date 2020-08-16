77f8edd854467affd01c60c0016fbd41830d8a38
daskelly45 <assigbid@gmail.com>
2020-08-16 16:38:56 +0000

    Added a bunch of ViewGroup or layout types (FrameLayout, LinerLayout, RelativeLayout, ConstraintLayout)

--- app/src/main/java/com/dave45/net/ad340/MainActivity.kt
+++ app/src/main/java/com/dave45/net/ad340/MainActivity.kt
@@ -5,9 +5,39 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+
+    // region Setup methods
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    // endregion Setup methods
+
+
+    // region Teardown methods
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    // endregion Teardown methods
 
 }
