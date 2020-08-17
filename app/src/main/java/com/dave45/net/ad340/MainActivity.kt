ba4c873729ffc84c23bcdce7c9cb8a75eb959341
daskelly45 <assigbid@gmail.com>
2020-08-17 12:58:50 +0000

    Week 2 ending. Created a small interactive activity.

--- app/src/main/java/com/dave45/net/ad340/MainActivity.kt
+++ app/src/main/java/com/dave45/net/ad340/MainActivity.kt
@@ -2,15 +2,33 @@ package com.dave45.net.ad340
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    // region Properties
 
+    var times = 0
+
+    //endregion Properties
 
     // region Setup methods
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val zipcodeEditText: EditText = findViewById(R.id.zipcodeEditText)
+        val enterButton: Button = findViewById(R.id.enterButton)
+        val locationIcon: ImageView = findViewById(R.id.locationIcon)
+        val titleTextView: TextView = findViewById(R.id.titleTextView)
+
+
+        enterButton.setOnClickListener {
+            val zipCode = zipcodeEditText.text.toString()
+            if(zipCode.length != 5)
+                Toast.makeText(this, R.string.incorrectZipCodeValueError, Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(this, "Zip code : \"$zipCode\"", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onStart() {
