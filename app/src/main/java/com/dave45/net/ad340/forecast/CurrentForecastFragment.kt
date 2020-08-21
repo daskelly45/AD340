4d4d23890ad605ed533d11ae3d222a76b9f9614d
daskelly45 <assigbid@gmail.com>
2020-08-21 15:14:58 +0000

    Removed some forgotten TODOs in file [Week 5 end]

--- app/src/main/java/com/dave45/net/ad340/forecast/CurrentForecastFragment.kt
+++ app/src/main/java/com/dave45/net/ad340/forecast/CurrentForecastFragment.kt
@@ -14,11 +14,6 @@ import com.dave45.net.ad340.*
 import com.dave45.net.ad340.details.ForecastDetailsActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [CurrentForecastFragment.newInstance] factory method to
