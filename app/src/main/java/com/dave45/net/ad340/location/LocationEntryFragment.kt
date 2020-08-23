9419587e498a17b78da8e7d6e63ba4a46e90e2f3
daskelly45 <assigbid@gmail.com>
2020-08-23 08:11:58 +0000

    Removed other forgotten TODOs in files [Week 6 start]

--- app/src/main/java/com/dave45/net/ad340/location/LocationEntryFragment.kt
+++ app/src/main/java/com/dave45/net/ad340/location/LocationEntryFragment.kt
@@ -13,11 +13,6 @@ import com.dave45.net.ad340.AppNavigator
 import com.dave45.net.ad340.R
 import kotlin.random.Random
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [LocationEntryFragment.newInstance] factory method to
@@ -55,24 +50,4 @@ class LocationEntryFragment : Fragment() {
 
         return view
     }
-
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment LocationEntryFragment.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            LocationEntryFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
 }
